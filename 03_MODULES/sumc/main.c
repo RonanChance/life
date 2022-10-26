73341ce2c8c7b80d35c4b2cf9afd960d077630e5
Ronan Chance <rcdonovan@wm.edu>
2022-10-26 18:20:44 -0400

    Class addition

---
+++ 03_MODULES/sumc/main.c
@@ -0,0 +1,32 @@
+// compute sum of consecutive integers 1 to n inclusive
+// git will track everything else
+
+#include <stdio.h>
+#include "mylib.h"
+
+long cumulative_sum(int N);
+
+int main(int argc, char *argv[])
+{
+int N;
+long result;
+// ask for a number
+printf("Enter a number:\n");
+scanf("%d", &N);
+
+// compute result
+result = cumulative_sum(N);
+// display result
+printf("The sum from 1 to %d (inclusive) is %ld\n", N, result);
+return 0;
+}
+
+long cumulative_sum(int N){
+long accumulator = 0;
+
+for (int n=1; n <= N; n++){
+accumulator += n;
+
+}
+return accumulator;
+}
