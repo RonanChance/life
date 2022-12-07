6351818e7b8d406fbc462cc1d99f9266afe2c494
Ronan Chance <rcdonovan@wm.edu>
2022-12-07 17:17:58 -0500

    Add java files from last class

---
+++ 00_HELLOJ/Hello.java
@@ -0,0 +1,14 @@
+class Model {
+	double weights[];
+	Model() {
+	System.out.println("Constructing a Model instance");
+	}
+}
+
+class Hello {
+	public static void main(String args[]) {
+		System.out.println("Hello, world");
+		System.out.println("Instantiating Model instance...");
+		Model model = new Model();
+	}
+}
